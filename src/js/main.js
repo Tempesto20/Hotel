a9000131c4c5fe826e0b57d3e37d7803358a6e59
Tempesto20 <Bro74rus1@mail.ru>
2021-07-12 00:35:33 +0300

    1

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import '../../node_modules/focus-visible/dist/focus-visible';
+import '../libs/jquery.min.js';
+
+import '../scss/main.scss';
+import '../index.html';
+
+
+// $('body').css('color', 'red');
