5f61a77a0013d11b58c65e596b8f582e36bbaef1
Tempesto20 <Bro74rus1@mail.ru>
2021-07-14 15:55:12 +0300

    jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import '../../node_modules/focus-visible/dist/focus-visible';
-import '../libs/jquery.min.js';
+
+import $ from 'jquery';
+
+//  import '../libs/jquery.min.js';
+// import $ from '../../node_modules/jquery/dist/jquery.min.js';
+
 import './script.js';
 import '../scss/main.scss';
+
 // import '../index.html';
 
-
-// $('body').css('color', 'red');
